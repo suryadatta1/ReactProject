e40422083b71c1e0287c6cf824512e125afc82b8
suryadatta1 <suryadattatangirala@outlook.com>
2019-12-21 16:19:49 +0530

    jsx

---
+++ indecission app/public/scripts/app.js
@@ -0,0 +1,13 @@
+"use strict";
+
+console.log("app");
+
+var template = React.createElement(
+  "h1",
+  null,
+  "Indecession  App"
+);
+
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
---
+++ indecission app/src/app.js
@@ -0,0 +1,7 @@
+console.log("app");
+
+var template = <h1>Indecession  App</h1>;
+
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
